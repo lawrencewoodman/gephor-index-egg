39a85c0de131a471625d24d81795f6cf1deb7399
Lawrence Woodman <238718+lawrencewoodman@users.noreply.github.com>
2025-03-18 21:13:59 +0000

    Use serve-path rather than serve-file serve-dir in serve-path/index

--- handler.scm
+++ handler.scm
@@ -13,19 +13,19 @@
 
 ;; Tries the following handlers in turn until one returns
 ;; non-false or the last one fails:
-;;   serve-index serve-file serve-dir
+;;   serve-index serve-path
 ;; Returns the value of the last handler tried.
 ;; TODO: Test
 (define (serve-path/index root-dir request)
   (any (lambda (h) (h root-dir request))
-       (list serve-index serve-file serve-dir) ) )
+       (list serve-index serve-path) ) )
 
 
 ;; If an 'index' file is in the directory formed from root-dir and request
 ;; process the index file and return the result.
 ;; See selector->local-path in gephor egg for more information about
 ;; selector requirements.
-;;  Returns #f if index file doesn't exist or can't be processed properly.
+;; Returns #f if index file doesn't exist or can't be processed properly.
 (define (serve-index root-dir request)
   (let ((selector (request-selector request))
         (client-address (request-client-address request)))
